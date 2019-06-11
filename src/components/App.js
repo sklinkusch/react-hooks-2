58cd3ab1824ab50b9accec1419e6ca04f82ab1b6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-11 19:36:59 +0200

    fixed code with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from '../images/logo.svg';
-import '../styles/App.css';
-import ProductListClass from './ProductListClass';
-// import ProductListHook from './ProductListHook';
+import React from "react";
+import logo from "../images/logo.svg";
+import "../styles/App.css";
+// import ProductListClass from './ProductListClass';
+import ProductListHook from "./ProductListHook";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ProductListClass />
-        {/* <ProductListHook /> */}
+        {/* <ProductListClass /> */}
+        <ProductListHook />
       </header>
     </div>
   );
--- src/components/ProductListHook.js
+++ src/components/ProductListHook.js
@@ -1,29 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ProductListHook = () => {
-  const [products] = useState(
-    [
-      {
-        id: 1,
-        name: "Doom"
-      },
-      {
-        id: 2,
-        name: "Quake"
-      }
-    ]
-  );
-  const [cart, setCart] = useState([])
-  const addItem = (item) => {
-    this.setCart((prevCart) => { cart: [...prevCart, item] })
-  }
-  return (<div>
-    <h2>Cart</h2>
-    {cart.map((item, index) => <div key={index}>{item.name}</div>)}
-    <h2>Products</h2>
-    {products.map(product => <div key={product.id} onClick={addItem(product)}>{product.name}</div>)
+  const [products] = useState([
+    {
+      id: 1,
+      name: "Doom"
+    },
+    {
+      id: 2,
+      name: "Quake"
     }
-  </div >);
-}
+  ]);
+  const [cart, setCart] = useState([]);
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cart.map((item, index) => (
+        <div key={index}>{item.name}</div>
+      ))}
+      <h2>Products</h2>
+      {products.map(product => (
+        <div key={product.id} onClick={() => addItem(product)}>
+          {product.name}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductListHook;
